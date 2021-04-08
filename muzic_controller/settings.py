29e65ee5bad7df6bfbfdfbb489a09de7f62ffd8c
Johann Alberts <alberts.johann@gmail.com>
2021-04-08 22:11:31 +0100

    Create api app and set up base urls and first simple test view

--- muzic_controller/settings.py
+++ muzic_controller/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
